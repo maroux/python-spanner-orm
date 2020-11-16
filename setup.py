92fcba49cd9752f14edbd5f89b158c990f7cbb66
Aniruddha Maru <maru@standard.ai>
2020-11-16 11:14:52 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name="spanner-orm",
-    version="0.3.2",
+    version="0.3.3",
     description="Basic ORM for Spanner",
     maintainer="Aniruddha Maru",
     maintainer_email="aniruddhamaru@gmail.com",
